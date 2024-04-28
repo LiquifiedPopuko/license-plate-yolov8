6073f189a4c68feda26c53f4c4270554817c1eb8
Thai <thaiphat_s@cmu.ac.th>
2024-04-28 23:42:51 +0700

    Commit 1

---
+++ training/dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# platedetectionv2 > 2022-12-02 6:11pm
+https://universe.roboflow.com/papinwit-rattanasiri-ynqke/platedetectionv2
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- training/train.py
+++ training/train.py
@@ -6,4 +6,4 @@ model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 #use the model
-results = model.train(data="D:/Code/SeniorProj/license-plate-yolov8/training/dataset/data.yaml", epochs=5, device=0) #train
+results = model.train(data="D:/Code/SeniorProj/license-plate-yolov8/training/dataset/data.yaml", epochs=100) #train
