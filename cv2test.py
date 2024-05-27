26b9a6c3d9a0e836ef5acc60d9fab2df44177070
Thai <thaiphat_s@cmu.ac.th>
2024-05-27 16:14:49 +0700

    Add experimental code

---
+++ cv2test.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+import util
+from ultralytics import YOLO
+
+cap = cv2.VideoCapture(0)
+
+#load models
+model_path = './model_testing/license_detector.pt'
+model = YOLO(model_path)  # pretrained YOLOv8n model
+
+results_path = './results/'
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    
+    # Press Q to detect license plate
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      i = 0
+      detections = model(frame)
+      for detection in detections[0].boxes.data.tolist():
+         x1, y1, x2, y2, score, class_id = detection
+
+         #crop license plate
+         img_path = results_path + str(i) + '.jpg'
+         i+=1
+         license_plate = frame[int(y1):int(y2), int(x1):int(x2), :]
+
+         #process license plate
+         license_plate = cv2.cvtColor(license_plate, cv2.COLOR_BGR2GRAY)
+         
+         result = util.read_license_plate(license_plate)
+         print(str(result))
+         #save crop
+         cv2.imwrite(img_path, license_plate)
+
+    # Press R to quit
+    if cv2.waitKey(1) & 0xFF == ord('r'):
+       break
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
