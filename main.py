7531560e15d629bd6895c659444c1611fcf6d1b9
Thai <thaiphat_s@cmu.ac.th>
2024-05-29 01:59:37 +0700

    Adjust indent

--- main.py
+++ main.py
@@ -20,20 +20,19 @@ i = 0
 #detections[0] is TEMPorary
 #license plate detection and crop
 for detection in detections[0].boxes.data.tolist():
-   x1, y1, x2, y2, score, class_id = detection
+    x1, y1, x2, y2, score, class_id = detection
 
-   #crop license plate
-   img_path = results_path + str(i) + '.jpg'
-   i+=1
-   license_plate = img[int(y1):int(y2), int(x1):int(x2), :]
+    #crop license plate
+    img_path = results_path + str(i) + '.jpg'
+    i+=1
+    license_plate = img[int(y1):int(y2), int(x1):int(x2), :]
 
-   #process license plate
-   license_plate = cv2.cvtColor(license_plate, cv2.COLOR_BGR2GRAY)
-   
-   result = util.read_license_plate(license_plate)
-   print(str(result))
-   #save crop
-   cv2.imwrite(img_path, license_plate)
-
+    #process license plate
+    license_plate = cv2.cvtColor(license_plate, cv2.COLOR_BGR2GRAY)
+    
+    result = util.read_license_plate(license_plate)
+    print(str(result))
+    #save crop
+    cv2.imwrite(img_path, license_plate)
 
 #write results
